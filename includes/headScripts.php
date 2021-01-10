6ae92eb716ea862d030f5c48fc8f208aee37165f
Pixelao <contacto@adrianmr.com>
2021-01-10 23:42:55 +0100

    Fixed error with moment.local comas

--- includes/headScripts.php
+++ includes/headScripts.php
@@ -2,4 +2,4 @@
     <link rel="stylesheet" href="dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="dist/css/global-custom.css">
     <script src="dist/js/moment-with-locales.min.js"></script>
-    <?= '<script>moment.locale(' . $_ENV['LANG'] . ');</script>' ?>
+    <?= '<script>moment.locale(\'' . $_ENV['LANG'] . '\');</script>' ?>
