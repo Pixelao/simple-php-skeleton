3f22950ba471f15400cee819d8ba5e0c302c89eb
Pixelao <contacto@adrianmr.com>
2021-01-10 17:28:03 +0100

    Added moment.js on default javascripts included

--- includes/headScripts.php
+++ includes/headScripts.php
@@ -1,2 +1,7 @@
     <!-- Scripts and CSS included in head -->
     <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <script src="dist/js/moment-with-locales.min.js"></script>
+
+    <script>
+        moment.locale('es');
+    </script>
